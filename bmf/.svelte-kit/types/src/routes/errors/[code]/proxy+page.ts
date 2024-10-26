c8490513f71ca4c4195ad5b9cf8c9a52bdeb85e4
Arthur Leon Fuller <52759634+alfullerdev@users.noreply.github.com>
2024-10-26 16:33:22 -0700

    beta 0.1

---
+++ bmf/.svelte-kit/types/src/routes/errors/[code]/proxy+page.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import type { NumericRange } from '@sveltejs/kit';
+
+export const load = ({ params }: Parameters<PageLoad>[0]) => {
+    const status = +params.code;
+
+    if (isNaN(status) || status < 400 || status > 599) {
+        throw error(500, 'Wrong code');
+    } else {
+        const code = status as NumericRange<400, 599>;
+        throw error(code, 'Not found');
+    }
+};
